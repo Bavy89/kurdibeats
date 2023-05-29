a95fd3322548878b4cbfb8fa76dfec0c6c46338d
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2023-05-29 23:48:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,60 @@ function hideSlide() {
 }
 
 showSlide();
+
+
+// Smooth scrolling function
+function smoothScroll(target, duration) {
+    var targetElement = document.querySelector(target);
+    var targetPosition = targetElement.offsetTop;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+  
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      var timeElapsed = currentTime - startTime;
+      var ease = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, ease);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+  
+    // Easing function
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+  
+    requestAnimationFrame(animation);
+  }
+  
+  // Add a class to the navigation menu when scrolling
+  var nav = document.querySelector("nav");
+  var navLinks = document.querySelectorAll("nav a");
+  
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 0) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
+  
+  // Smooth scroll when anchor tags are clicked
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      var target = this.getAttribute("href");
+      var duration = 1000; // Adjust this value to control the scroll speed
+  
+      // Check if the target is the top of the page
+      if (target === "#") {
+        smoothScroll("body", duration);
+      } else {
+        smoothScroll(target, duration);
+      }
+    });
+  });
+  
